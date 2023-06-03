09396e6d2a14b8869e6a9f01bbf6236e1d371b4e
Ajay Kumar <ajay.ak8302@gmail.com>
2023-06-03 20:08:53 +0530

    improvement fix 3

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -36,7 +36,7 @@ const NavBar = () => {
     <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed nav">
       <div>
         {/* <h1 className="text-5xl font-signature ml-2"><a className="link-underline hover:transition ease-in-out delay-150 hover:underline hover:decoration-solid" href="#">Ajay Kumar</a></h1> */}
-        <h1 className="text-4xl font-signature ml-2"><a className="link-underline link-underline-black"  target="_blank" rel="noreferrer">Ajay Kumar</a></h1>
+        <h1 className="text-4xl font-signature ml-2"><a className="link-underline link-underline-black" href="https://firstcommerce-dev.netlify.app/login" target="_blank" rel="noreferrer">Ajay Kumar</a></h1>
       </div>
 
       <ul className="hidden md:flex">
